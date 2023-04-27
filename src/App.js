d7dcfcc5079afa9d251f50f868cf3d1114003683
YenninM <yarethgracia@gmail.com>
2023-04-26 18:48:50 -0700

    Se agrega el proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [total,setTotal] = useState('');
+  const [propina,setPropina] = useState('10%');
+  const [dividir, setDividir] = useState(1);
+  const [dividirTotal, setDividirTotal]= useState(0);
+
+  function handleTotalCuentaChange(e){
+    setTotal(e.target.value);
+  }
+  
+  function handlePropinaChange(e){
+    let value = e.target.value.replace('%','');
+    if(value.indexOf('%') === -1) {
+      value = value+'%';
+    }
+    setPropina(value);
+  }
+  function dividirMenos(){
+    setDividir(oldValue => Math.max(oldValue - 1, 1));
+  }
+  function dividirMas(){
+    setDividir(oldValue => oldValue + 1);
+  }
+
+  function calculate(){
+    const percentage = 1 + parseInt(propina.replace('%','')) / 100;
+    const result = (total * percentage / dividir).toFixed(2);
+    setDividirTotal(result);
+  }
+
+  useEffect(() => {
+    calculate();
+  }, [total,propina,dividir])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>Total Cuenta</label>
+      <input type="text" placeholder={'0.00'} value={total}
+        onChange={handleTotalCuentaChange}/>
+      <label>Propina</label>
+      <input type="text" placeholder={'0.00'} value={propina}
+        onChange={handlePropinaChange}/>
+      <div className="summary">
+        <div className="split">
+        <label>Dividir</label>
+            <div className="split-control">
+            <button onClick={dividirMenos}>-</button>
+            <span>{dividir}</span>
+            <button onClick={dividirMas}>+</button>
+            </div>
+        </div>
+        <div className="result">
+          <label>Total por persona</label>
+          <span>{dividirTotal}</span>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
+
